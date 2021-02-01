f9279aea647ef0d168e6980aa131fbe6b4fd9cff
Michael Doctor <michaelcdoctor@gmail.com>
2021-01-31 23:16:46 -0700

    Added Navbar w/ no cart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home.page';
 
-import './App.css';
+import './doc.css';
 
 function App() {
   return (
--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Navbar from '../components/hoc/Navbar';
+import useScript from '../utils/useScript.hook';
 
 const Home = () => {
-  return <div>Home Page</div>;
+  useScript(
+    'https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5ff4fd881cc2cd35ee68ff88',
+  );
+  useScript(
+    'https://res.cloudinary.com/hsse18xji/raw/upload/v1612153620/HHA/js/webflow_vdcwdg.js',
+  );
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
