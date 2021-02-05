db41d5e1490a20aee828eac490dbc327e8eccc21
SooeunKim <sooeuk08@gmail.com>
2021-02-05 15:51:44 -0700

    Added product details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home.page';
 import Products from './pages/Products.page';
+import ProductDetail from './pages/ProductDetail.page';
 import NotFound from './pages/NotFound.page'
 
 import './doc.css';
@@ -12,6 +13,7 @@ function App() {
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/products" component={Products} />
+      <Route exact path="/products/detail" component={ProductDetail} />
       <Route component={NotFound} />
     </Switch>
   );
