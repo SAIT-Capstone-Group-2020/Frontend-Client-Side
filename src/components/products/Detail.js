9c755c8967a413ef21398fa71845fae471f91b65
SooeunKim <sooeuk08@gmail.com>
2021-02-23 04:33:59 -0700

    Changed route path for Product Detail page

--- src/components/products/Detail.js
+++ src/components/products/Detail.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Detail = () => {
+  const { productID } = useParams();
+
   return (
     <div classNameName="section cc-product-detail">
     <div className="product-image" style={{"backgroundImage":`url(${"https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.i-scmp.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F768x768%2Fpublic%2Fd8%2Fimages%2Fmethode%2F2020%2F10%2F13%2Fea674f86-076b-11eb-afc8-92e0da0ef1c3_image_hires_135401.jpg%3Fitok%3DEUh-DxEg%26v%3D1602568447&imgrefurl=https%3A%2F%2Fwww.scmp.com%2Fmagazines%2Fstyle%2Fcelebrity%2Farticle%2F3104886%2Fk-pop-idol-turned-k-drama-star-bae-suzy-5-things-about&tbnid=mvuEaS4HExIihM&vet=12ahUKEwjwqYbZ6NPuAhVKADQIHW9KAegQMygBegUIARC1AQ..i&docid=HD4SRN3z7V0_1M&w=768&h=768&q=suzy&ved=2ahUKEwjwqYbZ6NPuAhVKADQIHW9KAegQMygBegUIARC1AQ"})`}}></div>
     <div className="product-details-wrap">
       <div className="product-detail-main-details">
         <h1 className="product-detail-name">Product Name</h1>
-        <div className="paragraph-light">Product Description</div>
+        <div className="paragraph-light">{ productID }</div>
       </div>
       <div className="divider cc-dark-divider"></div>
       <div className="product-info">
